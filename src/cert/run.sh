524b599939a49cd90e2f0ed04e30e9de204192ad
Mike Jewell <mike.jewell@sceneskope.com>
2017-10-18 09:46:12 +0100

    More changes for Docker - adding cert container to handle ssl setup

---
+++ src/cert/run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+trap "exit" SIGHUP SIGINT SIGTERM
+
+if [ -z "$DOMAINS" ] ; then
+	echo "No certificates required."
+	echo "Exiting"
+	exit 1
+fi
+CHECK_FREQ="${CHECK_FREQ:-10}"
+
+CERTBOT_DOMAINS="-d ${DOMAINS/ / -d }"
+
+check() {
+	echo "Checking certificates"
+
+    certbot certonly --webroot --text -w /app --preferred-challenges=http --agree-tos --email ${EMAIL} ${CERTBOT_DOMAINS} --post-hook "/renewed.sh"
+
+	echo "Checking again in $CHECK_FREQ days"
+	sleep ${CHECK_FREQ}d
+	check
+}
+
+check
