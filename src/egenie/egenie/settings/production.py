af165f3ace65605143873d6112febbf7933582d1
Tom Hinton <tom.hinton@cse.org.uk>
2019-03-13 11:25:16 +0000

    Add screen order to production settings

--- src/egenie/egenie/settings/production.py
+++ src/egenie/egenie/settings/production.py
@@ -22,3 +22,6 @@ MEDIA_URL = '/media/'
 SECRET_KEY = 'spinge'
 
 ALLOWED_HOSTS = ['*']
+
+ONSITE_SCREEN_ORDER = ['alwayson', 'temperature', 'annotation', 'pinboard:public']
+OFFSITE_SCREEN_ORDER = ['alwayson', 'temperature', 'annotation']
