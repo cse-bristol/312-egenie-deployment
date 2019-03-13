8fee4261bd226b7b1499f23dcdf1e3298e93fc9d
Tom Hinton <tom.hinton@cse.org.uk>
2019-03-13 10:18:33 +0000

    Add underscore

--- src/egenie/egenie/middleware.py
+++ src/egenie/egenie/middleware.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 class CTechMiddleware(object):
     """ Some URLs require an anonymous user, especially sdstore and annotation creation. This middleware replaces the logged in user in these instances."""
     def process_request(self, request):
-        if (request.path.startswith('/sdstore/') and request.method == 'GET') or (request.path.startswith('/annotation/') and request.method == 'POST'):
+        if (request.path.startswith('/sd_store/') and request.method == 'GET') or (request.path.startswith('/annotation/') and request.method == 'POST'):
             request.user = User.objects.get(username='anonymous')
